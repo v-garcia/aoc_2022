29d6b137aff0767d8f8beef8eb0848f89e57fb90
Vincent Garcia <VGarcia@sierrawireless.com>
2022-12-10 14:17:29 +0100

    Add day10

---
+++ day10.clj
@@ -0,0 +1,33 @@
+(ns day9
+  (:require [clojure.string :as str]))
+
+
+(def input
+  (->> (slurp "day10.input")
+       (str/split-lines)
+       (map (fn [l] (let [[command n] (str/split l #" ")] [(keyword command) (and n (Integer/parseInt n))])))))
+
+(def cycle->v
+  (->> input
+       (reduce (fn [[v :as l] [command n]]
+                 (case command
+                   :noop (conj l v)
+                   :addx (conj l v (+ v n))))
+         '(1))
+       reverse
+       vec))
+
+;; Question 1
+
+(def answer1
+  (->> [20 60 100 140 180 220]
+       (mapv #(* % (nth cycle->v (dec %))))
+       (apply +)))
+
+;; Question 2
+
+(->> cycle->v
+     (map-indexed (fn [i v] (if 
+                             (<= (dec v) (mod i 40) (inc v)) \# \.)))
+     
+     (partition 40))
